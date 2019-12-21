21963079f52c96ba7624787314bf96855541c638
vfrmn <vitaliy.furman19@gmail.com>
2019-12-21 23:50:54 +0200

    delete all

--- build/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: /Users/vfrmn/Documents/development/sdks/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
--- build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/vfrmn/Documents/development/flutter-projects/virtual_class/build/ios/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
