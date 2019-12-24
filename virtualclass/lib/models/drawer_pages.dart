8a43fe86638152e4b5df7085c3ce528e703db69e
vfrmn <vitaliy.furman19@gmail.com>
2019-12-24 05:57:21 +0200

    new project

---
+++ virtualclass/lib/models/drawer_pages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:virtualclass/main.dart';
+
+class Home1 extends ChangeNotifier{
+  String text;
+
+void draw(){
+  //Navigator.push(context, MaterialPageRoute(builder: (context)=> MyHomePage(title: 'NewTest')));
+}
+
+}
---
+++ virtualclass/lib/screens/notifications.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:virtualclass/models/model_app_settings.dart';
+import 'package:virtualclass/models/navigation_model.dart';
+
+
+class Notifications extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => NotificationsState();
+}
+
+class NotificationsState extends State<Notifications> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: MyTheme.myTheme['screen'],
+      body: Center(child: Container(child: Column(
+        children: <Widget>[
+          IconButton(icon: Icon(Icons.arrow_back), onPressed: () {Navigator.pop(context);},),
+          Checkbox(
+                                onChanged: (bool value) {
+                                  setState(() {
+                                    Provider.of<Navigation>(context, listen: false).setBool(value);
+                                  });
+                                },
+                                value: Provider.of<Navigation>(context, listen: false).val,
+                              ),
+        ],
+      ))),
+    );
+  }
+}
