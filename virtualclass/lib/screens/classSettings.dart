7ef85eb515e594a0a8f9436f389ced8bfc5c5233
h4rrold <voronkov.g4@gmail,com>
2020-01-07 16:25:32 +0200

    Rename screen files

---
+++ virtualclass/lib/screens/classSettings.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:virtualclass/models/model_app_settings.dart';
+import 'package:virtualclass/models/navigation_model.dart';
+
+
+class MyProfile extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _MyProfileState();
+  
+    
+    }
+  
+  class _MyProfileState extends State<MyProfile> {
+  @override
+  Widget build(BuildContext context) {
+    print('rebuild Settings');
+    return 
+           Scaffold(
+            // drawer: AppDrawer(),
+            // appBar: AppBar(
+              
+            //   title: Text('Settings'),
+            // ),
+            body: Form(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    
+                  ),
+                  Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                  child: IconButton(
+                    icon: Icon(Icons.notifications),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/notifications');
+                    },
+                  ),
+                )
+                ],
+              )
+
+            ),
+       // backgroundColor: MyTheme.myTheme['screen'],
+        
+      
+    );
+  }
+}
+  
